bcb731a278f1e95a9754a9c7b831affb804c5bec
Jared Sy <jaredblase@gmail.com>
2020-08-20 22:26:31 +0800

    Basic structure of a binary tree

---
+++ src/BST.java
@@ -0,0 +1,29 @@
+class Node {
+    Node left;
+    Node right;
+    String word;
+}
+
+public class BST {
+    private Node root;
+
+    public BST() {
+
+    }
+
+    public boolean search() {
+        return false;
+    }
+
+    public void insert() {
+
+    }
+
+    public void inOrder() {
+
+    }
+
+    public void destroy() {
+
+    }
+}
