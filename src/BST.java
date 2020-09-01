b0f6375e718194ecf0dbf1fb79b2a88e5fd40afc
Jared Sy <jaredblase@gmail.com>
2020-09-01 19:00:51 +0800

    Added basic function implementations.

--- src/BST.java
+++ src/BST.java
@@ -1,7 +1,26 @@
+// package-private class and methods
 class Node {
-    Node left;
-    Node right;
-    String word;
+    private Node left;
+    private Node right;
+    private final String word;
+    private int count;
+
+    Node(String word) {
+        this.word = word;
+        count = 1;
+    }
+
+    int getCount() {
+        return count;
+    }
+
+    String getWord() {
+        return word;
+    }
+
+    void addCount() {
+        count++;
+    }
 }
 
 public class BST {
@@ -11,16 +30,22 @@ public class BST {
 
     }
 
-    public boolean search() {
+    public boolean search(String word) {
         return false;
     }
 
-    public void insert() {
+    public void insert(String word) {
 
     }
 
     public void inOrder() {
+        Node node = new Node("Hello");      // placeholder
 
+        // traverse left
+
+        System.out.printf("%30s\t%s", node.getWord(), node.getCount());
+
+        // traverse right
     }
 
     public void destroy() {
